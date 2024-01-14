e050310d337587d4a65d0f4bbc72e1d6cecdfa13
Samara Vitória <devsamaravitoria@gmail.com>
2024-01-14 18:25:07 -0300

    fix: Adição do contador para a exibição de itens distintos

--- imdb250/imdb250/spiders/imdb.py
+++ imdb250/imdb250/spiders/imdb.py
@@ -6,9 +6,11 @@ class ExampleSpider(scrapy.Spider):
     start_urls = ['https://letterboxd.com/andrewcult/list/im-just-a-girl-in-the-world/detail/']
 
     def parse(self, response):
+        cont = 0
         for filmes in response.xpath('//h2//a/text()'):
             yield{
-                'filmes': response.xpath('//h2//a[contains(@href, "/film/")]/text()'),
-                'ano': response.xpath('//h2//a[contains(@href, "/films/")]/text()')
+                'filmes': response.xpath('//h2//a[contains(@href, "/film/")]/text()')[cont].get(),
+                'ano': response.xpath('//h2//a[contains(@href, "/films/")]/text()')[cont].get()
             }
+            cont+=1
 
